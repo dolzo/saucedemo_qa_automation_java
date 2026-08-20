159a24c49502565cec152ba4b1d03b967ce0ca07
dolzo <degolxz01@gmail.com>
2026-08-20 18:29:50 -0400

    test(login): crear test para verificar el login con un usuario no registrado

--- src/test/java/automation/LoginTests.java
+++ src/test/java/automation/LoginTests.java
@@ -4,6 +4,8 @@ import data.DataGiver;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.LoginPage;
@@ -12,6 +14,7 @@ import utilities.Logs;
 
 public class LoginTests extends BaseTest {
 
+    private static final Logger log = LoggerFactory.getLogger(LoginTests.class);
     private final LoginPage loginPage = new LoginPage();
 
     @BeforeMethod(alwaysRun = true)
@@ -38,5 +41,17 @@ public class LoginTests extends BaseTest {
 
         loginPage.verifyErrorMessage(lockedCredentials.getMessage());
 
+    }
+
+    @Test(groups = {regression, smoke})
+    @Description("Se verifica que aparezca un mensaje de error al hace login con un usuario que no se encuentre registrado")
+    @Severity(SeverityLevel.CRITICAL)
+    public void unexistentUserTest(){
+        final var unexistentCredentials = DataGiver.getUnexistentCredentials();
+        loginPage.fillLoginForm(
+                unexistentCredentials.getUsername(),
+                unexistentCredentials.getPassword()
+        );
+        loginPage.verifyErrorMessage(unexistentCredentials.getMessage());
     }
 }
