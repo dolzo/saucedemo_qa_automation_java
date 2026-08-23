c88eedb48d753030573746a3374575bfd3ad77da
dolzo <degolxz01@gmail.com>
2026-08-22 21:48:25 -0400

    feat(data-provider): agregar data provider para validacion de mensajes de error de usuario

--- src/test/java/data/CustomDataProviders.java
+++ src/test/java/data/CustomDataProviders.java
@@ -1,10 +1,12 @@
 package data;
 
+import models.User;
 import org.testng.annotations.DataProvider;
 
 public class CustomDataProviders {
 
     public static final String DP_CREDENTIALS = "dpCredentials";
+    public static final String DP_MESSAGES = "dpMessages";
 
     @DataProvider(name = DP_CREDENTIALS)
     public Object[][] credentialsDataProvider(){
@@ -15,6 +17,19 @@ public class CustomDataProviders {
                 {locked.getUsername(), locked.getPassword(), locked.getMessage()},
                 {unexistent.getUsername(), unexistent.getPassword(), unexistent.getMessage()}
         };
+    }
+
+    @DataProvider(name = DP_MESSAGES)
+    public Object[][] errorMessageDataProvider(){
+        final var user = new User(); // rellenado con faker
+        final var errorMessageMap = Parser.getErrorMessageMap(); // conseguido del excel
+
+        return new Object[][]{
+                {"", user.getLastName(), user.getPostalCode(), errorMessageMap.get("error_name").getMensaje()}, // sin el primer nombre
+                {user.getName(), "", user.getPostalCode(), errorMessageMap.get("error_lastname").getMensaje()}, // sin el apellido
+                {user.getName(), user.getLastName(), "", errorMessageMap.get("error_zipcode").getMensaje()} // sin el codigo postal
+        };
+
     }
     
 }
