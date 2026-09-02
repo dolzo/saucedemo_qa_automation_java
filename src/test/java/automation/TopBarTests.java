64fb71dca34809b80de65098e5248c70480d7238
dolzo <degolxz01@gmail.com>
2026-09-02 19:54:21 -0400

    feat(topbar): crear clase TopBarTests para probar funcionalidades de la topbar

---
+++ src/test/java/automation/TopBarTests.java
@@ -0,0 +1,36 @@
+package automation;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.ItemDetailPage;
+import pages.TopBar;
+import utilities.BaseTest;
+
+public class TopBarTests extends BaseTest {
+
+    private final TopBar topBar = new TopBar();
+    private final ItemDetailPage itemDetailPage = new ItemDetailPage();
+
+    @BeforeMethod
+    public void setUp() {
+        commonFlows.goToItemDetail("Sauce Labs Fleece Jacket");
+    }
+
+    @Test(groups = {regression})
+    @Description("Se verifica que al agregar un producto al carrito vacio, el numero sobre el icono de ese cambie a 1")
+    @Severity(SeverityLevel.MINOR)
+    public void verifyProductIcon() {
+        itemDetailPage.clickAddToCart();
+        topBar.clickBurgerMenu();
+
+        softAssert.assertEquals(
+                topBar.getCartBadgeNumber(),
+                "1"
+        );
+
+        softAssert.assertAll();
+    }
+}
