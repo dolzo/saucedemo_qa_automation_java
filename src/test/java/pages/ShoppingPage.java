d3ce662950106859f9d6f9ada2e8db4508209a4a
dolzo <degolxz01@gmail.com>
2026-09-05 19:14:22 -0400

    feat(shopping): agregar metodo en ShoppingPage para hacer clic en todos los botones add to cart

--- src/test/java/pages/ShoppingPage.java
+++ src/test/java/pages/ShoppingPage.java
@@ -19,6 +19,7 @@ public class ShoppingPage extends BasePage {
     private final By selectItem = By.cssSelector("select[data-test='product-sort-container']");
     private final By itemNames = By.className("inventory_item_name");
     private final By itemPrices = By.className("inventory_item_price");
+    private final By addToCartButtons = By.className("btn_inventory");
 
     private By getProductPrice(String itemName){
         return RelativeLocator
@@ -92,6 +93,18 @@ public class ShoppingPage extends BasePage {
         }
 
         return prices;
+    }
+
+    @Step("Haciendo clic en add to cart de todos los items")
+    public void clickAllAddToCart(){
+        Logs.info("Haciendo clic en add to cart de todos los items");
+
+        List<WebElement> addToCartList = findAll(addToCartButtons);
+
+        for (WebElement element : addToCartList){
+            element.click();
+        }
+
     }
 
 }
