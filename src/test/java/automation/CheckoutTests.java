84b3aee66e11e164b10d213aa120b2d98cd1d74b
dolzo <degolxz01@gmail.com>
2026-09-09 20:51:23 -0300

    test(checkout): agregar caso de prueba para validar cancelacion de orden en CheckoutTests

--- src/test/java/automation/CheckoutTests.java
+++ src/test/java/automation/CheckoutTests.java
@@ -1,16 +1,29 @@
 package automation;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import pages.CheckoutPage;
+import pages.ShoppingPage;
 import utilities.BaseTest;
 
 public class CheckoutTests extends BaseTest {
 
-    CheckoutPage checkoutPage = new CheckoutPage();
+    private final CheckoutPage checkoutPage = new CheckoutPage();
+    private final ShoppingPage shoppingPage = new ShoppingPage();
 
     @BeforeMethod
     public void setUp() {
         commonFlows.goToCheckoutPage();
     }
 
+    @Test(groups = {regression})
+    @Description("Verificar que al cancelar una orden en el checkout se redirija a la pagina de shopping")
+    @Severity(SeverityLevel.NORMAL)
+    public void verifyCancelAtCheckoutTest() {
+        checkoutPage.clickCancelButton();
+        shoppingPage.verifyPage();
+    }
 }
