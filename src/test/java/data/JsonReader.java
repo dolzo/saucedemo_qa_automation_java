0bde8af6098f415c05e0b5aacba382216aea259c
dolzo <degolxz01@gmail.com>
2026-09-12 01:42:13 -0300

    feat(data-reader): implementar metodo getJsonProductImageMap para retornar map con metadata de imagenes

--- src/test/java/data/JsonReader.java
+++ src/test/java/data/JsonReader.java
@@ -2,6 +2,7 @@ package data;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import models.CredentialJson;
+import models.ProductImageJson;
 import utilities.Logs;
 
 import java.io.File;
@@ -10,6 +11,7 @@ import java.io.IOException;
 public class JsonReader {
 
     private final static String credentialsPath = "src/test/resources/data/credenciales.json";
+    private final static String productImagePath = "src/test/resources/data/productImages.json";
 
     public static CredentialJson getJsonCredentialsMap(){
         final var objectMapper = new ObjectMapper();
@@ -21,6 +23,20 @@ public class JsonReader {
             );
         }catch (IOException ioException){
             Logs.error("Error al leer el Json de credenciales: %s", ioException.getLocalizedMessage());
+            throw new RuntimeException(ioException.getLocalizedMessage());
+        }
+    }
+
+    public static ProductImageJson getJsonProductImageMap(){
+        final var objectMapper = new ObjectMapper();
+
+        try {
+            return objectMapper.readValue(
+                    new File(productImagePath),
+                    ProductImageJson.class
+            );
+        }catch (IOException ioException){
+            Logs.error("Error al leer el Json de imagenes de productos: %s", ioException.getLocalizedMessage());
             throw new RuntimeException(ioException.getLocalizedMessage());
         }
     }
